dd181f5ceec61fb6420adafbf36179e927313424
Wellerson <wellerson.coffee@gmail.com>
2019-03-26 11:20:31 -0300

    add hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Navbar from './Navbar'
 import RecipeItem from './RecipeItem'
 import recipes from '../sample_data/recipes.json'
 import helpFilter from './../helpers/filter'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.recipes = recipes.results;
-    this.state = {
-      searchString: ''
-    };
+const App = () => {
+  const [searchString, setSearchString] = useState('')
+  const recipesItems = recipes.results
 
+  const getValue = (value) => {
+    setSearchString(value);
   }
-
-  // getValue = (value) =>{
-  //   this.setState({ searchString: value });
-  // }
  
-  render() { 
-    return (
-      <div className="App">
-        <Navbar getValue={this.getValue} value={this.state.searchString} />
-        <div className="container mt-10">
-          <div className="row">
-            {
-              helpFilter(this.recipes, this.state.searchString, RecipeItem)
-            }
-          </div>
+  return (
+    <div className="App">
+      <Navbar getValue={getValue} value={searchString} />
+      <div className="container mt-10">
+        <div className="row">
+          {
+            helpFilter(recipesItems, searchString, RecipeItem)
+          }
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import logo from '../logo.svg';
 
 const Navbar = ({ value, getValue }) => {
-    const [input, setInput] = useState(value) 
     return (
         <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
             <div className="navbar-brand col-1">
@@ -11,8 +10,8 @@ const Navbar = ({ value, getValue }) => {
 
             <div className="form-group justify-content-center row col-10 my-2">
                 <input
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
+                    value={value}
+                    onChange={(e) => getValue(e.target.value)}
                     className="form-control col-9 mr-2"
                     type="search"
                     placeholder="Search"
