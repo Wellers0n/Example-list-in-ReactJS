1a92765e8afb7562ae52a6e641f1a0b4bcf9398f
Wellerson <wellerson.coffee@gmail.com>
2019-03-26 11:26:04 -0300

    set value

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ const App = () => {
  
   return (
     <div className="App">
-      <Navbar getValue={setValue} value={searchString} />
+      <Navbar setValue={setValue} value={searchString} />
       <div className="container mt-10">
         <div className="row">
           {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import logo from '../logo.svg';
 
-const Navbar = ({ value, getValue }) => {
+const Navbar = ({ value, setValue }) => {
     return (
         <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
             <div className="navbar-brand col-1">
@@ -11,7 +11,7 @@ const Navbar = ({ value, getValue }) => {
             <div className="form-group justify-content-center row col-10 my-2">
                 <input
                     value={value}
-                    onChange={(e) => getValue(e.target.value)}
+                    onChange={(e) => setValue(e.target.value)}
                     className="form-control col-9 mr-2"
                     type="search"
                     placeholder="Search"
