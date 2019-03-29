efcfe3fe13aceac81e10332631b7fe693c50bef0
Wellerson <wellerson.coffee@gmail.com>
2019-03-29 18:48:02 -0300

    add <mark> in the filds

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import logo from '../logo.svg';
 
 const Navbar = ({ value, setValue }) => {
--- src/helpers/filter.js
+++ src/helpers/filter.js
@@ -4,12 +4,31 @@ function fil(arr, query, Value){
     return arr.map((item, index) => {
         if(item.title.toLowerCase().indexOf(query.toLowerCase()) > -1 || 
             item.ingredients.toLowerCase().indexOf(query.toLowerCase()) > -1 ){
-            return <Value key={index} title={item.title}
-                            ingredients={item.ingredients} thumbnail={item.thumbnail}
-                            link={item.href}                         
-                    />
+
+                let partsTitle = item.title.split(new RegExp(`(${query})`, 'gi'));
+                let valorTitle = partsTitle.map((part, index) => {
+                    if(part.toLowerCase() === query.toLowerCase()){
+                        return <mark key={index}>{part}</mark>
+                    }else{
+                      return <span key={index}>{part}</span>  
+                    }
+                })
+
+                let partsIngre = item.ingredients.split(new RegExp(`(${query})`, 'gi'));
+                let valorIngre = partsIngre.map((part, index) => {
+                    if(part.toLowerCase() === query.toLowerCase()){
+                        return <mark key={index}>{part}</mark>
+                    }else{
+                      return <span key={index}>{part}</span>  
+                    }
+                })
+
+                return <Value key={index} title={valorTitle}
+                                ingredients={valorIngre} thumbnail={item.thumbnail}
+                                link={item.href}                         
+                        />
         }
     })
 }
 
-export default fil
+export default fil  
